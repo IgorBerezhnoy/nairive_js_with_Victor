d497458d10fc07ce18338d11916400692d10999c
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-08-17 02:20:57 +0800

    finished lesson 13

---
+++ lesson 13/main.js
@@ -0,0 +1,15 @@
+// // let car = {
+// //     color: 'Red',
+// //     getColor() {
+// //         const a = () => {
+// //             console.log(this.color);
+// //         };
+// //         a();
+// //     }
+// // };
+// // car.getColor();
+//
+//
+// const user={
+//     name
+// }
